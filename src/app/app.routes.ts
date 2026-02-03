81df27dd49906feb5592765015adf352fe860272
Raymundo Sanchez <raymundosanchez62@gmail.com>
2026-02-02 22:18:02 -0700

    feat: complete static mockup and temporary routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HeroDetail } from './pages/hero-detail/hero-detail';
+import { HomePage } from './pages/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePage },
+  { path: 's', component: HeroDetail },
+];
